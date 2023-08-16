b0b98dab80a58a39afa74149d74d7949b2b87b5f
berkanbasaran05 <berkanbasaran05@gmail.com>
2023-08-16 20:39:49 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import pic from './assets/mb.jpg'
+import pic from './assets/ey.jpeg'
 
 function App() {
   const [points, setPoints] = useState([])
@@ -32,23 +32,24 @@ function App() {
   }
   
   return (
-    <div> 
-    <div className='screen' onClick={clickHandle}>
+    <div className='screen' style={{ backgroundImage: `url(${pic})`, backgroundSize: 'cover' }}> 
+   <iframe width="923" height="519" src="https://www.youtube.com/embed/6Cuki8mezDM" title="ZAKKUM // Seni Rastgele Sevmedim (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div className='screen' onClick={clickHandle} >
        <header className='header'>
-        <button disabled={points.length == 0} onClick={redoHandle}>Redo</button>
-        <button disabled={data.length == 0} onClick={undoHandle}>Undo</button>
+        <button disabled={points.length == 0} onClick={redoHandle}>Geri Al</button>
+        <button disabled={data.length == 0} onClick={undoHandle}>İleri</button>
 
          </header>
 
       {points.map((heart ,key) => ( 
       
-      <div className='heart items-center flex' key={key} style={{ left:heart.x, top:heart.y }}> <div className='text'> Maike Berkan </div>  </div>
+      <div className='heart items-center flex' key={key} style={{ left:heart.x, top:heart.y }}> <div className='text'> Eda Yasin </div>  </div>
 
       ))}
       
       
       </div>
-      <div> <img src={pic} className='img'></img></div>
+
       </div>
   )
 }
